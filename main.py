18653314017caaf0887e654f2a47851f52bbb889
victorryakh <56445224+victorryakh@users.noreply.github.com>
2024-02-07 02:50:29 +0300

    Reading vars from user

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-# This example requires the 'message_content' intent.
-
 import config
+import os
 import discord
+from discord.ext import commands
+
+import sys
+import logging
+log_handler = logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("matematch.log", encoding='utf-8', mode='w'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+async def setup_hook():
+    bot.tree.copy_global_to(guild=discord.Object(id=config.guild_id))
+    await bot.tree.sync(guild=discord.Object(id=config.guild_id))
+    logging.info(f"Tree synced to guild {config.guild_id}")
+bot.setup_hook = setup_hook
+
+async def wait_message(context):
+     def check(message):
+        return message.channel == context.channel and message.author == context.author
+     return await bot.wait_for('message', check=check)
+bot.wait_message = wait_message
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    logging.info('The bot is ready!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.check
+def check_commands(ctx):
+    logging.info(f"Context author is bot: {ctx.author.bot}")
+    return ctx.author.bot == False
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.hybrid_command()
+async def register(ctx):
+    """Выполни, чтобы начать использовать бота"""
+    await ctx.send("🔹 Дата рождения\nВведи дату рождения в формате DD.MM.YYYY (например, 06.02.2024)")
+    birthday = (await bot.wait_message(ctx)).content
+    await ctx.send("🔹 О себе\nНапиши ниже то, что стоит знать твоему тиммейту о тебе")
+    about_me = (await bot.wait_message(ctx)).content
+    
 
-client.run(config.token)
+bot.run(config.token, log_handler=log_handler)
---
+++ model.py
@@ -0,0 +1,4 @@
+def is_registered(user_id: int):
+    # TODO: make a function to fast check registration
+    print(f"{user_id} is not registered")
+    return False
